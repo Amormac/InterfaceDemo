4e6d00fe813038fdfd89943e9d74eaa7c3188a0f
Ariel Moreira <gonzalomoreira1939@gmail.com>
2024-12-16 19:37:50 -0500

    Proyecto de interfaces terminado

--- src/ec/edu/puce/interfaces/Main.java
+++ src/ec/edu/puce/interfaces/Main.java
@@ -4,7 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hola puce");
+		
+		Rectangulo rectangulo1 = new Rectangulo();
+		rectangulo1.setAltura(10);
+		rectangulo1.setBase(5);
+		rectangulo1.setNombre("rectangulo 1");
+		System.out.println("El area del " + rectangulo1.getNombre() + " es: " + rectangulo1.calcularArea());
+		
+		Circulo circulo1 = new Circulo();
+		circulo1.setNombre("circulo 1");
+		circulo1.setRadio(6);
+		System.out.println("El area del " + circulo1.getNombre() + " es: " + circulo1.calcularArea());
+		
+		Triangulo triangulo1 = new Triangulo();
+		triangulo1.setAltura(5);
+		triangulo1.setBase(10);
+		triangulo1.setNombre("triangulo 1");
+		System.out.println("El area del " + triangulo1.getNombre() + " es: " + triangulo1.calcularArea());
 
 	}
+	
 
 }
